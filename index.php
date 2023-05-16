527b10517a304cdff206379d7f3cb85f2674bda7
peppeluc22 <gluca0260@gmail.com>
2023-05-16 15:46:07 +0200

    fixed main structure

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
-
+include './Models/Movie.php';
 ?>
 
 <?php include './components/header.php'; ?>
 
-<?php include './components/main.php'; ?>
+<main class="overflow-hidden">
+    <div class="container py-5">
+        <div class="row">
+            <?php foreach ($movies as $movie) { ?>
+                <?php include './components/card.php'; ?>
+            <?php } ?>
+        </div>
+    </div>
+</main>
+
+
 
 <?php include './components/footer.php'; ?>
